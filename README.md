c8c21be5dc5044dab7d680e028e1507626953ebe
Liubomyr Mykhalchenko <liubko.qwert@gmail.com>
2017-01-10 04:39:41 +0800

    Add eSpeak, improve logs

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 
+# Requires
+
+`mplayer`
+`ALSA`
+`espeak`
+`aplay`
+
 # Run
 
 in order to run on startup we will add a cron job
@@ -10,4 +17,20 @@ crontab -e
 ```
 # add job
 @reboot /home/liubko/music-in-the-shower/main.sh
+```
+
+# eSpeak
+
+requires `espeak` and `aplay`
+
+```
+espeak --stdout 'Hello' | sudo aplay
+```
+
+# Volume
+
+to adjust volume use
+
+```
+sudo alsamixer
 ```
--- main.sh
+++ main.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-# setup logs
-LOG_FILE=LOGS.log
-exec > >(tee -a ${LOG_FILE} )
-exec 2> >(tee -a ${LOG_FILE} >&2)
-
-# adds timestamp before echo
-echo_time() {
-    date +"%R $*"
-}
-
 # init pin
 sudo bash -c 'echo 203 > /sys/class/gpio/export'
 sudo bash -c 'echo in > /sys/class/gpio/gpio203/direction'
 
+LOG_FILE="`dirname $0`/LOGS.txt"
 IS_MUSIC_PLAYING=false
 URL="http://air.aristocrats.fm:8000/live1"
 PIN_ADDRESS="/sys/class/gpio/gpio203/value"
 SLEEP_TIME=2
 
-echo_time "[Start music-in-the-shower service]"
+# adds timestamp before echo and writes to file
+echo_time() {
+    date +"%D %R $*" >> $LOG_FILE
+}
+
+echo_time "\n\n[Start music-in-the-shower service]"
 while true; do 
     # read pin value
     LIGHT_PIN_VALUE=$(<$PIN_ADDRESS)
 
     if [ $LIGHT_PIN_VALUE -eq 0 ] && [ $IS_MUSIC_PLAYING == false ] ; then
-        # Light is ON
+        espeak --stdout "Welcome to the Bathroom" | sudo aplay
+        espeak --stdout "Let's play some music now" | sudo aplay
+
+       # Light is ON
         echo_time "[Start music]"
         
         # To play `*.mp3` use `mpg123`
         # sudo mpg123 test_sound.mp3 &
         
         # run music in background and save PID
-        sudo mplayer -vo null -ao alsa $URL &
+        sudo mplayer -vo null -ao alsa $URL &>/dev/null &
         PROC_ID=$!
 
         IS_MUSIC_PLAYING=true
@@ -41,8 +40,9 @@ while true; do
         echo_time "[Stop Music]" 
         sudo kill $PROC_ID
         IS_MUSIC_PLAYING=false
+        espeak --stdout "Sorry to see you go. Bye." | sudo aplay
     else
-        echo_time "[Idle]"
+        echo_time "[Idle | IS_MUSIC_PLAYING=$IS_MUSIC_PLAYING | LIGHT_PIN_VALUE=$LIGHT_PIN_VALUE]"
     fi
 
     sleep $SLEEP_TIME
