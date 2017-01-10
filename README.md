ef24912cd6ccf84c82d7d2d81fe2bae72ce90f1c
Liubomyr Mykhalchenko <liubko.qwert@gmail.com>
2017-01-10 19:59:24 +0200

    Update Readme

--- README.md
+++ README.md
@@ -1,38 +1,44 @@
+# Requirements
 
-# Requires
+## Hardware
 
-`mplayer`
-`ALSA`
-`espeak`
-`aplay`
+- [NanoPI M1 512Mb RAM](http://arduino-ua.com/prod1569-nanopi-m1-512mb-ram)
+- [Sunny 5В microUSB 2,1A](http://arduino-ua.com/prod1591-blok-pitaniya-5v-microusb-2-1a)
+- [MicroSD card 16GB Class 10](http://arduino-ua.com/prod869-microsd-karta-silicon-power-16gb-class-10--adapter)
+- [Photoresistor](http://arduino-ua.com/prod1213-modyl-datchika-osveshhennosti)
+- [Wi-Fi adapter Ralink (optional)](http://arduino-ua.com/prod576-Miniaturnii_Wi-Fi_adapter_Ralink)
+- Speakers
+
+## Software
+
+Installed `Debian GNU/Linux 8.6 (jessie)` _([How to install](http://wiki.friendlyarm.com/wiki/index.php/NanoPi_M1#Make_an_Installation_TF_Card))_
+
+Also you have to install:
+- `mplayer`
+- `ALSA`
+- `espeak`
+- `aplay`
 
 # Run
 
-In order to run on startup we will add a root cron job.
+Several shortcut commands available
+```
+make start // start service
+make stop // stop service
+make volume // run alsamixer to adjust volume
+```
 
-This opens up root's crontab. We want to invoke script as root.
+# Autorun
+
+In order to run on startup add a __root__ cron job.
+
+To edit crontab jobs.
 
 ```
 sudo crontab -e
 ```
 
 ```
-# Run job on reboot, but with delay(to be sure that network is accesible).
+# Run job on reboot, but with delay(to make sure that network is accesible).
 @reboot sleep 60 && /home/liubko/music-in-the-shower/music-in-the-shower.sh
-```
-
-# eSpeak
-
-requires `espeak` and `aplay`
-
-```
-espeak --stdout 'Hello' | sudo aplay
-```
-
-# Volume
-
-to adjust volume use
-
-```
-sudo alsamixer
 ```
--- music-in-the-shower.sh
+++ music-in-the-shower.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 LOG_FILE="`dirname $0`/LOGS.txt"
-IS_MUSIC_PLAYING=false
 URL="http://air.aristocrats.fm:8000/live1"
 PIN_ADDRESS="/sys/class/gpio/gpio203/value"
 SLEEP_TIME=2
+IS_MUSIC_PLAYING=false
 
 # adds timestamp before echo and writes to file
 echo_time() {
@@ -13,48 +13,50 @@ echo_time() {
 
 echo_time "[Start music-in-the-shower service]"
 
-# init pin
+# might need to spin a while on startup untill it will be possible to export pin
 LIGHT_PIN_VALUE=""
 while [ -z $LIGHT_PIN_VALUE ]; do # check if empty string
     echo_time "[Init Pin]"
-    
+
     sudo bash -c 'echo 203 > /sys/class/gpio/unexport'
     sudo bash -c 'echo 203 > /sys/class/gpio/export'
     sudo bash -c 'echo in > /sys/class/gpio/gpio203/direction'
-    
+
     # read pin value
     LIGHT_PIN_VALUE=$(<$PIN_ADDRESS)
-    # echo_time "[Pin value is: $LIGHT_PIN_VALUE]"
+
+    echo_time "[Pin value is: $LIGHT_PIN_VALUE]"
 done
 
-while true; do 
+# main loop
+while true; do
     # read pin value
     LIGHT_PIN_VALUE=$(<$PIN_ADDRESS)
 
     if [ $LIGHT_PIN_VALUE -eq 0 ] && [ $IS_MUSIC_PLAYING == false ] ; then
+        # light is ON, and music isn't playing -> we need to START music
+        echo_time "[Start music]"
+
         espeak --stdout "It's nice to see you!" | sudo aplay
 
-       # Light is ON
-        echo_time "[Start music]"
-        
         # To play `*.mp3` use `mpg123`
         # sudo mpg123 test_sound.mp3 &
-        
+
         # run music in background and save PID
         sudo mplayer -vo null -ao alsa $URL &>>$LOG_FILE &
         PROC_ID=$!
 
         IS_MUSIC_PLAYING=true
     elif [ $LIGHT_PIN_VALUE -eq 1 ] && [ $IS_MUSIC_PLAYING == true ] ; then
-        # Light is OFF
-        echo_time "[Stop Music]" 
+        # light is OFF, but music is still playing -> we need to STOP music
+        echo_time "[Stop Music]"
+
         sudo kill $PROC_ID
         IS_MUSIC_PLAYING=false
+
         espeak --stdout "Sorry to see you go. Bye." | sudo aplay
-    # else
-        # echo_time "[Idle | IS_MUSIC_PLAYING=$IS_MUSIC_PLAYING | LIGHT_PIN_VALUE=$LIGHT_PIN_VALUE]"
     fi
 
     sleep $SLEEP_TIME
-done 
+done
 
